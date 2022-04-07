0e56c3d3bcc498c78b0aad0c26266f3777805118
Philip Homburg <philip@nlnetlabs.nl>
2022-04-07 11:37:07 +0200

    Proto6

---
+++ proto6/connectbyname.h
@@ -0,0 +1,28 @@
+/*
+connectbyname.h
+
+Interface for connectbyname
+*/
+
+#include <getdns/getdns.h>
+
+struct cbn_context
+{
+	struct event_base *event_base;
+	getdns_context *getdns_ctx;
+};
+
+struct bufferevent;
+typedef void (*cbn_callback_T)(struct bufferevent *bev, void *ref);
+
+int cbn_init(struct cbn_context *cbn_ctx, struct event_base *event_base);
+void cbn_clean(struct cbn_context *cbn_ctx);
+
+int connectbyname(struct cbn_context *cbn_ctx,
+	const char *hostname, const char *servname, int *fdp);
+
+int connectbyname_asyn(struct cbn_context *cbn_ctx,
+	const char *hostname, const char *servname,
+	cbn_callback_T user_cb, void *user_ref, void **refp);
+
+void connectbyname_free(void *ref);
